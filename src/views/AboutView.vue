b7af14d83b060a3fcd6b47291c9f0c14ad7ccf02
Onumah Kalu Samuel <onumahkalusamuel@gmail.com>
2024-05-29 14:42:28 +0100

    first commit

---
+++ src/views/AboutView.vue
@@ -0,0 +1,8 @@
+<template>
+  <div class="bg-stone-500 h-screen flex">
+    <div class="p-20 max-w-[1200px] m-auto text-center bg-stone-800 text-white">
+      <div class="mb-5">This is an about page</div>
+      <router-link to="/" class="btn btn-secondary">Home page</router-link>
+    </div>
+  </div>
+</template>
---
+++ src/views/HomeView.vue
@@ -0,0 +1,14 @@
+<script setup lang="ts">
+import MoneyBagIcon from "@/components/icons/MoneyBagIcon.vue";
+</script>
+
+<template>
+  <div class="text-8xl text-center py-20">
+    <div class="flex items-center justify-center gap-5">
+      Home Page
+      <money-bag-icon class="text-red-600 h-20 w-20"/>
+    </div>
+    <hr class="my-10"/>
+    <router-link to="/about" class="btn btn-lg">About page</router-link>
+  </div>
+</template>
