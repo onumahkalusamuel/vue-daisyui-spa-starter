04571399e05794b8cb6c10bdda781170fa4213d2
Onumah Kalu Samuel <onumahkalusamuel@gmail.com>
2024-06-04 09:54:06 +0100

    added scroll behavior to router

--- src/router/index.ts
+++ src/router/index.ts
@@ -4,6 +4,21 @@ import MainLayout from "@/layouts/MainLayout.vue";
 
 const router = createRouter({
     history: createWebHistory(import.meta.env.BASE_URL),
+    scrollBehavior(to, __, savedPosition) {
+        if (savedPosition) {
+            return savedPosition
+        } else if (to.hash) {
+            return {
+                el: to.hash,
+                behavior: "smooth",
+            }
+        } else {
+            return {
+                top: 0,
+                behavior: "smooth",
+            }
+        }
+    },
     routes: [
         {
             path: '/',
